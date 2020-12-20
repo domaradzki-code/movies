782fb3d46455efd9858ec65d89e1e712ec63fef7
domaradzki-azure <piotr.z.domaradzki@gmail.com>
2020-12-20 21:03:19 +0000

    Db: MSSQL container init

---
+++ mssql/create-database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE DemoData;
+GO
+USE DemoData;
+GO
+CREATE TABLE Movies (Title nvarchar(max), Released int, Genre nvarchar(max), Director nvarchar(max), [User] nvarchar(max), Timestamp datetime);
+GO
