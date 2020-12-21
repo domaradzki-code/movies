df7bc64e4476c7ea8b2c178f77285355cdb6d7ab
domaradzki-code <piotr.z.domaradzki@gmail.com>
2020-12-21 01:11:52 +0100

    Db: MSSQL operations added, switch added

--- src/db/dbApi.ts
+++ src/db/dbApi.ts
@@ -2,5 +2,23 @@
  * This module is an API to abstract DB implementation from its interface. Currently it's using 
  * filesystem as database, but should be switched for some real one.
  */
+import { createMovieFs, getMoviesFs, countMoviesMonthlyFs, clearDbFs } from './fileStorageDb';
+import {createMovieMssql, getMoviesMssql, countMoviesMonthlyMssql} from './msSqlDb'
 
-export { createMovie, getMovies, countMoviesMonthly, clearDb } from './fileStorageDb';
+export let createMovie: Function
+export let getMovies: Function
+export let countMoviesMonthly: Function
+export let clearDb: Function
+
+if (process.env.DB_TYPE == 'MSSQL') {
+    createMovie = createMovieMssql
+    getMovies = getMoviesMssql
+    countMoviesMonthly = countMoviesMonthlyMssql
+    clearDb = () => {}
+}
+else {
+    createMovie = createMovieFs
+    getMovies = getMoviesFs
+    countMoviesMonthly = countMoviesMonthlyFs
+    clearDb = clearDbFs
+}
